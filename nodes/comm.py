c011d43a022a5183386e688eca7daadf962cfbd0
maciekbaczmanski <maciej.baczmanski@gmail.com>
2023-01-14 23:41:34 +0100

    added telepot bot

--- nodes/comm.py
+++ nodes/comm.py
@@ -1,6 +1,10 @@
 from pygame import mixer  # Load the popular external library
 import paho.mqtt.client as mqtt
 import time
+import telepot
+from telepot.loop import MessageLoop
+
+
 mixer.init()
 mixer.music.load('ryszard.mp3')
 broker_address = "172.16.25.128"
@@ -23,6 +27,44 @@ client.connect(broker_address, 1883)
 client.loop_start()
 client.subscribe([("alarm", 0), ])
 
+
+
+
+class Telegram:
+    def __init__(self,token):
+        self.bot = telepot.Bot(token)
+        self.permissions = [1255224844, 1990072643]
+        MessageLoop(self.bot, self.handle).run_as_thread()
+
+    def msg_me(self, message):
+        self.bot.sendMessage(1255224844, message)
+
+    def msg_all(self, message):
+        for ID in self.permissions:
+            self.bot.sendMessage(ID, message)
+
+    def handle(self, msg):
+        if msg['from']['id'] in self.permissions:
+            if '/ping' in msg['text']:
+                self.bot.sendMessage(msg['from']['id'], 'Pong!')
+            elif '/add' in msg['text'] and len(msg['text']) > 5:
+                try:
+                    toadd = int(msg['text'].replace('/add ', ''))
+                    self.permissions.append(toadd)
+                    self.bot.sendMessage(msg['from']['id'], 'User added!')
+                except:
+                    self.bot.sendMessage(msg['from']['id'], 'Unknown error while adding user!')
+            else:
+                self.msg_all(
+                    'Wrong message! If want to respond to last email, type:\nSPACE,BUILDING   (example: 05,9A)\nnone  <- for custom without space&building')
+        else:
+            self.bot.sendMessage(msg['from']['id'],
+                                 'No permisions! Ask admin to add your ID.\nYour ID:\n' + str(msg['from']['id']))
+
+
+token=open("/home/pi/token.txt").read()
+bocik = Telegram(token)
+bocik.msg_all('Service starting after system restart!\n For help type: /help')
 
 while True:
     time.sleep(1)
