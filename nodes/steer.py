638b7f26c0a205b9f94184c919d966516ccc75d8
maciekbaczmanski <maciej.baczmanski@gmail.com>
2023-01-14 17:12:33 +0100

    add while loop for reading

--- nodes/steer.py
+++ nodes/steer.py
@@ -1,4 +1,5 @@
 import evdev
+import time
 devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
 for device in devices:
     print(device.path, device.name, device.phys)
@@ -8,12 +9,17 @@ mem_values = {"ABS_X":128 , "ABS_BRAKE" : 0, "ABS_GAS": 0}
 
 device = evdev.InputDevice('/dev/input/event3')
 print(device)
-for event in device.read_loop():
-    # if event.type == evdev.ecodes.EV_KEY:
-    
-    # print(evdev.ecodes.ABS[event.code])
-    if event.type == evdev.ecodes.EV_ABS and evdev.ecodes.ABS[event.code] in mem_values.keys():
-        mem_values[evdev.ecodes.ABS[event.code]] = event.value
-        print(mem_values)
-        # print(evdev.categorize(event))
-        # print(evdev.ecodes.ABS[event.code], " : ",event.value)
+while True:
+    time.sleep(0.1)
+    try:
+        for event in device.read():
+            # if event.type == evdev.ecodes.EV_KEY:
+            
+            # print(evdev.ecodes.ABS[event.code])
+            if event.type == evdev.ecodes.EV_ABS and evdev.ecodes.ABS[event.code] in mem_values.keys():
+                mem_values[evdev.ecodes.ABS[event.code]] = event.value
+                print(mem_values)
+                # print(evdev.categorize(event))
+                # print(evdev.ecodes.ABS[event.code], " : ",event.value)
+    except Exception as e:
+        print(e)
