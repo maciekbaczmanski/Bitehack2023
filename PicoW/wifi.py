b23159d5531a6bc31c5324abc75232dfabf2bc99
Adrian Besciak <adrian.besciak99@gmail.com>
2023-01-14 22:09:10 +0100

    added motor driver

--- PicoW/wifi.py
+++ PicoW/wifi.py
@@ -3,8 +3,13 @@ import network
 def connect():
   SSID = 'MamNaCiebieOko'
   PASSWD = 'Niezgadniesz.'
-  
+
   wlan = network.WLAN(network.STA_IF)
   wlan.active(True)
   wlan.connect(SSID, PASSWD)
+  while wlan.status() != 3 or not wlan.isconnected():
+    print("Connecting to WiFi network")
+    pass
+  print("Succesfully connected to WiFi")
+
   return wlan
