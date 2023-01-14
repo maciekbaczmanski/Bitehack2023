515fa67c7d738d973dffe306141ef9ea4a946a72
maciekbaczmanski <maciej.baczmanski@gmail.com>
2023-01-14 17:17:18 +0100

    final printing version

--- nodes/steer.py
+++ nodes/steer.py
@@ -10,7 +10,7 @@ mem_values = {"ABS_X":128 , "ABS_BRAKE" : 0, "ABS_GAS": 0}
 device = evdev.InputDevice('/dev/input/event3')
 print(device)
 while True:
-    time.sleep(1)
+    time.sleep(0.5)
     print("-------------------------")
     try:
         for event in device.read():
@@ -19,8 +19,8 @@ while True:
             # print(evdev.ecodes.ABS[event.code])
             if event.type == evdev.ecodes.EV_ABS and evdev.ecodes.ABS[event.code] in mem_values.keys():
                 mem_values[evdev.ecodes.ABS[event.code]] = event.value
-                print(mem_values)
                 # print(evdev.categorize(event))
                 # print(evdev.ecodes.ABS[event.code], " : ",event.value)
     except:
         pass
+    print(mem_values)
