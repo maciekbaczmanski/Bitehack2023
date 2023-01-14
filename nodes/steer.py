2eb2cb6dcff7fda0732a831bf63c721a4ff3987a
maciekbaczmanski <maciej.baczmanski@gmail.com>
2023-01-14 19:56:31 +0100

    button check

--- nodes/steer.py
+++ nodes/steer.py
@@ -24,8 +24,9 @@ while True:
         for event in device.read():
             print(evdev.categorize(event))
             # if event.type == evdev.ecodes.EV_KEY:
-            if event.type == evdev.ecodes.EV_KEY:
-                print(evdev.ecodes.EV_KEY[event.code])
+            if event.type == evdev.ecodes.EV_BTN:
+                print("aaa")
+                # print(evdev.ecodes.EV_KEY[event.code])
             # print(evdev.ecodes.ABS[event.code])
             if event.type == evdev.ecodes.EV_ABS and evdev.ecodes.ABS[event.code] in mem_values.keys():
                 mem_values[evdev.ecodes.ABS[event.code]] = event.value
