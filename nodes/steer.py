cc807183680b18929e7051cc7e8036c7b61f0588
maciekbaczmanski <maciej.baczmanski@gmail.com>
2023-01-14 16:06:13 +0100

    printing all pad events

--- nodes/steer.py
+++ nodes/steer.py
@@ -7,7 +7,8 @@ device = evdev.InputDevice('/dev/input/event3')
 print(device)
 for event in device.read_loop():
     # if event.type == evdev.ecodes.EV_KEY:
-    print(evdev.categorize(event))
+    
     # print(evdev.ecodes.ABS[event.code])
-
-    print(event.code, " : ",event.type, " : ",event.value)
+    if event.type == evdev.ecodes.EV_ABS:
+        print(evdev.categorize(event))
+        print(event.code, " : ",event.value)
