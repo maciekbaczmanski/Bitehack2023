bab20a02dd70709a1215f1264baeec5f5feadabd
Adrian Besciak <adrian.besciak99@gmail.com>
2023-01-14 23:30:45 +0100

    added controller watchdog to motor_driver

--- PicoW/motor_driver_main.py
+++ PicoW/motor_driver_main.py
@@ -8,16 +8,22 @@ mqtt_server = '172.16.25.128'
 client_id = 'motorDriver'
 motor_topic = 'steer/motors'
 
+msg_counter_full = 3000
+msg_counter_diff = 500
+msg_counter = 0
+
 motor2 = Motor(0, 1)
 motor1 = Motor(2, 3)
 
 def msgCallback(topic, message):
+    global msg_counter
     print(topic.decode('ascii'), message.decode('ascii'))
     if topic.decode('ascii') == motor_topic:
         print("Message: ", message)
         motor_speeds = json.loads(message.decode('ascii'))
         motor1.run(int(motor_speeds["L"]))
         motor2.run(int(motor_speeds["R"]))
+        msg_counter = msg_counter_full
 
 wifi = WiFi.connect()
 
@@ -26,6 +32,23 @@ client.connect()
 client.set_callback(msgCallback)
 client.subscribe(motor_topic)
 
-while True:
-    client.wait_msg()
+def mqttReceiveLoop():
+    while True:
+        client.wait_msg()
+
+from utime import sleep_ms
+import _thread
+
+def watchdog_loop():
+    while True:
+        global msg_counter
+        msg_counter -= msg_counter_diff
+        if msg_counter < 0:
+            motor1.run(0)
+            motor2.run(0)
+            print("Motors stopped due to controller inactivity")
+        sleep_ms(msg_counter_diff)
+
+_thread.start_new_thread(watchdog_loop, ())
+mqttReceiveLoop()
 
