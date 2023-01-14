ae052a3c9bccbc93e4f47f633da4c0e6c60917d7
maciekbaczmanski <maciej.baczmanski@gmail.com>
2023-01-14 14:33:49 +0100

    printing device statev2

--- nodes/steer.py
+++ nodes/steer.py
@@ -3,7 +3,7 @@ devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
 for device in devices:
     print(device.path, device.name, device.phys)
 
-device = evdev.InputDevice('/dev/input/event1')
+device = evdev.InputDevice('/dev/input/event0')
 print(device)
 for event in device.read_loop():
     if event.type == evdev.ecodes.EV_KEY:
